98c3515b768d0034e289cb6a6f73146e1e551dd5
zang <myginnygo@gmail.com>
2020-11-06 01:29:43 +0800

    使 axios 預設能抓到 csrf token

--- public/js/app.js
+++ public/js/app.js
@@ -19361,6 +19361,7 @@ window._ = __webpack_require__(/*! lodash */ "./node_modules/lodash/lodash.js");
 
 window.axios = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common['X-CSRF-TOKEN'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
