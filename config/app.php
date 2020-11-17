3f704fe63a0d05a11dc64e7c09f8abc40f7126bf
zang <myginnygo@gmail.com>
2020-11-18 00:42:33 +0800

    將 timezone 設定 改回 世界協調時間 (Coordinated Universal Time)

--- config/app.php
+++ config/app.php
@@ -67,8 +67,8 @@ return [
     |
     */
 
-    // 'timezone' => 'UTC',
-    'timezone' => 'Asia/Taipei',
+    'timezone' => 'UTC',
+    // 'timezone' => 'Asia/Taipei',
 
     /*
     |--------------------------------------------------------------------------
