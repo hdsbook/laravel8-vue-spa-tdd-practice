3ba42743cc7169961510ec68c8ecb13cc0ea76d7
zang <myginnygo@gmail.com>
2020-11-17 23:41:23 +0800

    有 token 時才進行 fetchUser

--- resources/js/middlewares/checkAuth.js
+++ resources/js/middlewares/checkAuth.js
@@ -1,7 +1,10 @@
 import store from '../store';
 
 export default async function (to, from, next) {
-  if (!store.state['auth']['user']) {
+  if (
+    !store.state['auth']['user'] &&  // 無登入使用者資料 => 進行 fetchUser
+    store.state['auth']['token']     // 但需要有 token 才能 fetchUser
+  ) {
     try {
       await store.dispatch('auth/fetchUser');
     } catch (e) { }
