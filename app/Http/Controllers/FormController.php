031fe0b17d4b4f5a6b91570e26a1f16509c98620
zang <myginnygo@gmail.com>
2020-11-27 22:02:42 +0800

    add FormController

---
+++ app/Http/Controllers/FormController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Form;
+use App\Repositories\FormRepository;
+use Illuminate\Http\Request;
+
+/**
+ * class FormController
+ *
+ * @property FormRepository $formRepo
+ */
+class FormController extends Controller
+{
+    protected $formRepo;
+
+    public function __construct(FormRepository $formRepo)
+    {
+        $this->formRepo = $formRepo;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $id = null;
+        $form = $this->formRepo->createForm($request);
+        $id = $form ? $form->id : null;
+
+        return response()->json([
+            'success' => (bool) $id,
+            'id' => $id,
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Form  $form
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Form $form)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Form  $form
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Form $form)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Form  $form
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Form $form)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Form  $form
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Form $form)
+    {
+        //
+    }
+}
