2eb9c4fe274cfef6636fb79b8d3c9c698e8f5ccd
zang <myginnygo@gmail.com>
2020-11-27 22:08:37 +0800

    add FormRepository

---
+++ app/Repositories/FormRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Form;
+use Illuminate\Http\Request;
+
+/**
+ * class FormRepository
+ *
+ * @property \App\Models\Form $model
+ */
+class FormRepository extends EloquentRepository
+{
+    public $model;
+
+    public function __construct(Form $model)
+    {
+        parent::__construct($model);
+    }
+
+    public function validateRequest(Request $request)
+    {
+        return $request->validate([
+            'form_name' => 'required',
+            'form_template_id' => 'required',
+        ]);
+    }
+
+    public function createForm(Request $request)
+    {
+        $data = $this->validateRequest($request);
+        return $request->user()
+            ? $request->user()->createdForms()->create($data)
+            : false;
+    }
+}
